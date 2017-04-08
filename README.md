569876ee1b0f0613ea51eb21ec08bb76e83d0c2a
Dmitry Monakhov <dmonakhov@openvz.org>
2017-04-08 11:50:47 +0300

    add Dockerfile

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # docker-image--smatch
 container with smatch (syntax checker for C) 
+
+
+## Usage examples
+
+Run smatch kernel check and save result at the source directory
+```
+docker run -v ./my_kernel_src:/bld/src -e BLD_RESULT=/bld/src \
+       dmonakhov/bld-smatch smatch_test_kernel
+```
+
+## Environment variables
+
+- `BLD_SOURCE		default: /bld/src`
+- `BLD_RESULT		default: /bld/result}`
+- `BLD_SMATCH_LOG	default: {BLD_RESULT}/smatch.log}`
+- `BLD_SMATCH_WLOG	default: {BLD_RESULT}/smatch.warn.log}`
+- `BLD_SMATCH_WLOG_ENV	default: {BLD_RESULT}/smatch.warn.env}`
---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+
+
+: ${BLD_SOURCE:=/bld/src}
+: ${BLD_RESULT:=/bld/result}
+: ${BLD_SMATCH_LOG:=${BLD_RESULT}/smatch.log}
+: ${BLD_SMATCH_WLOG:=${BLD_RESULT}/smatch.warn.log}
+: ${BLD_SMATCH_WLOG_ENV:=${BLD_RESULT}/smatch.warn.env}
+: ${BLD_ACTION:=${1}} # default action to first argument
+
+
+export PATH=$PATH:/usr/share/smatch/smatch_scripts
+
+function smatch_test_kernel
+{
+	# use default if not given
+	local target=""
+	if [ ! -z "$1" ]; then
+	    target="--target $1"
+	fi
+	pushd $BLD_SOURCE
+	time test_kernel.sh --endian --log $BLD_SMATCH_LOG --wlog $BLD_SMATCH_WLOG $target
+	NR_WARN=`wc -l $BLD_SMATCH_WLOG | gawk '{ print $1 }'`
+	echo "NR_WARN=$NR_WARN" > $BLD_SMATCH_WLOG_ENV
+	echo "SMATCH_NR_WARN=$NR_WARN" >> $BLD_SMATCH_WLOG_ENV
+	echo "Finish. Found $NR_WARN warnings"
+	popd
+}
+
+mkdir -p ${BLD_RESULT}
+
+case "$BLD_ACTION" in
+	smatch_test_kernel)
+		smatch_test_kernel
+		;;
+	## TODO add userspace tools compilation
+	## TODO add sparse kernel compilation
+	## TODO add real kernel compilation
+esac
+exit 0
